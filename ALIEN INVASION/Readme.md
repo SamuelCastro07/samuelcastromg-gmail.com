1629689e535f7b86b9960bf71f90de4391be6fde
Rodrigo Almeida Pirôpo <rodrigoap1307@gmail.com>
2020-03-30 01:17:06 -0300

    New game ALIEN_INVASION

---
+++ ALIEN INVASION/Readme.md
@@ -0,0 +1,8 @@
+# ALIEN INVASION
+## Jogo feito com a blibioteca pygame do Python
+
+<a href="https://www.pygame.org/docs/">DOCUMENTAÇÃO PYGAME</a>
+<p>Livro de refêrencia: Python Crash Course: A Hands-On, Project-Based Introduction to Programming</p>
+
+<p>Quem quiser jogar essa obra de arte precisa ter o pygame instalado...</p>
+<p>Para iniciar o game é so dar um run no arquivo <strong>alien_invasion.py</strong></p>
---
+++ ALIEN INVASION/game_stats.py
@@ -0,0 +1,17 @@
+class GameStats:
+    """Armazena dados estatisticos da invasão alienigena"""
+
+    def __init__(self, ai_settings):
+        """Inicializa os dados estatisticos"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        # Inicia a invasao alienigena em um estado ativo
+        self.game_active = True
+
+        # Inicia o jogo em um estado inativo
+        self.game_active = False
+
+    def reset_stats(self):
+        """Inicializa os dados estatisticos que podem mudar durante o jogo."""
+        self.ship_left = self.ai_settings.ship_limit
+        self.score = 0
